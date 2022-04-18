7b10a0c290fbc67504402afcda5fa767c2c58c1c
jasocar <jsotoc@ieselcaminas.org>
2022-04-18 13:22:13 +0200

    añado es par

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -3,3 +3,10 @@ def suma(x,y):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
     return x + y
+
+def espar(x):
+    if a % 2 == 0:
+    return True
+else:
+    return False
+
